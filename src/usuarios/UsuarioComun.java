0177148b245be1953b8e1c8ed7d3dc6df595f740
danteboe <juandavidtap38@gmail.com>
2024-04-13 14:14:52 -0500

    Added the Rol rol parameter into the Usuario constructor

--- src/usuarios/UsuarioComun.java
+++ src/usuarios/UsuarioComun.java
@@ -3,6 +3,7 @@ package usuarios;
 import java.util.ArrayList;
 import java.util.List;
 
+import auth.Rol;
 import logica.Factura;
 import piezas.Pieza;
 
@@ -10,8 +11,8 @@ public class UsuarioComun extends Usuario {
 	private List<Pieza> historial = new ArrayList<Pieza>();
 	private List <Pieza>piezasActuales = new ArrayList<Pieza>();
 	private List<Factura>compras = new ArrayList<Factura>();
-	public UsuarioComun(String id, String nombre, String apellido, String email, String password, String login, List<Pieza> historial, List<Pieza> piezasActuales, List<Factura> compras) {
-		super(id, nombre, apellido, email, password, login);
+	public UsuarioComun(String id, String nombre, String apellido, String email, String password, String login, Rol rol, List<Pieza> historial, List<Pieza> piezasActuales, List<Factura> compras) {
+		super(id, nombre, apellido, email, password, login, rol);
 		this.historial = historial;
 		this.piezasActuales = piezasActuales;
 		this.compras = compras;
