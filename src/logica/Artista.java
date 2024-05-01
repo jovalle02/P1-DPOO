407c1a8ad8ef4238eed20dbbcb96ccdb5d2e4771
danteboe <juandavidtap38@gmail.com>
2024-05-01 13:17:52 -0500

    change directory to Artista

--- src/logica/Artista.java
+++ src/logica/Artista.java
@@ -1,11 +1,10 @@
-package artistas;
+package logica;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import logica.Factura;
 import piezas.Pieza;
 
 public class Artista {
