996b7791161b00f70d9fe719a70e4f4f49b14eb8
danteboe <juandavidtap38@gmail.com>
2024-05-01 13:12:04 -0500

    implemented Artista class

---
+++ src/artistas/Artista.java
@@ -0,0 +1,40 @@
+package artistas;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import logica.Factura;
+import piezas.Pieza;
+
+public class Artista {
+	private String nombre;
+	private Map<String, Pieza>piezasHechas;
+	/* Se almacenarán las compras que involucren piezas del artista*/
+	private List<Factura> piezasVendidas; 
+	public Artista(String nombre) {
+		super();
+		this.nombre = nombre;
+		this.piezasHechas = new HashMap<String, Pieza>();
+		this.piezasVendidas = new ArrayList<Factura>();
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public Map<String, Pieza> getPiezasHechas() {
+		return piezasHechas;
+	}
+	public List<Factura> getPiezasVendidas() {
+		return piezasVendidas;
+	}
+	
+	public void agregarPiezaVendida(Factura factura) {
+		piezasVendidas.add(factura);
+	}
+	public void agregarPiezaHecha(Pieza pieza) {
+		piezasHechas.put(pieza.getId(), pieza);
+	}
+	
+}
+
